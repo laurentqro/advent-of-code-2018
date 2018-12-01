db42e561f25aba7b643dfe80945b19469d47b271
Laurent Curau <laurentcurau@gmail.com>
2018-12-01 14:26:03 +0000

    Day 1 - part 1

---
+++ src/advent_of_code_2018/day_1/day_1.clj
@@ -0,0 +1,17 @@
+(ns advent-of-code-2018.day-1.day-1)
+
+(def input
+  (slurp "./src/advent_of_code_2018/day_1/input.txt"))
+
+(defn- text->array [input]
+  (clojure.string/split input #"\n"))
+
+(defn- sum [numbers]
+  (apply + numbers))
+
+(defn resulting-frequency []
+  (->> input
+       (text->array)
+       (map #(Integer/parseInt %))
+       (sum)))
+
